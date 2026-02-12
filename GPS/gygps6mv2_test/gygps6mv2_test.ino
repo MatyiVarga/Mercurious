95b4b6347749943482f6cc899fa39461348f90b3
tamash <szasz.fabian@szekelymikokollegium.ro>
2026-02-12 21:48:18 +0200

    Refactor GPS variable name for consistency

--- GPS/gygps6mv2_test/gygps6mv2_test.ino
+++ GPS/gygps6mv2_test/gygps6mv2_test.ino
@@ -2,7 +2,7 @@
 #include <TinyGPS++.h>
 
 SoftwareSerial gpsSerial(8, 9);
-TinyGPSPlus gps;
+TinyGPSPlus GPS;
 
 unsigned long lastPrint = 0;
 
@@ -14,7 +14,7 @@ void setup() {
 void loop() {
   // 1. Constantly feed the GPS object
   while (gpsSerial.available()) {
-    gps.encode(gpsSerial.read());
+    GPS.encode(gpsSerial.read());
   }
 
   // 2. Print every 1000ms (1 Hz)
@@ -22,27 +22,27 @@ void loop() {
     lastPrint = millis(); 
 
     Serial.print("Satellites: ");
-    Serial.println(gps.satellites.value());
+    Serial.println(GPS.satellites.value());
 
     // Check 1: Has it ever found a location?
-    if (!gps.location.isValid()) {
+    if (!GPS.location.isValid()) {
       Serial.println("Status: Waiting for GPS Fix...");
     }
     // Check 2: Is the data fresh? (younger than 1.5 seconds)
-    else if (gps.location.age() > 1500) {
+    else if (GPS.location.age() > 1500) {
       Serial.println("Status: Signal Lost! (Data is stale)");
       Serial.print("Last valid age: ");
-      Serial.print(gps.location.age());
+      Serial.print(GPS.location.age());
       Serial.println(" ms ago");
     }
     // If valid and fresh, print coordinates
     else {
       Serial.print("Status: Active\n");
       Serial.print("Latitude: ");
-      Serial.println(gps.location.lat(), 6);
+      Serial.println(GPS.location.lat(), 6);
       
       Serial.print("Longitude: ");
-      Serial.println(gps.location.lng(), 6);
+      Serial.println(GPS.location.lng(), 6);
     }
     
     Serial.println("---------------------");
